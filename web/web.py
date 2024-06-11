ea17f34724cbb237934b38b0c7ba35138ac39355
尹铁成 <yintiecheng@yintiechengdeMacBook-Pro.local>
2024-06-11 17:03:58 +0800

    add dockerfile2

--- web/web.py
+++ web/web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello_world():
-    return "<p>Hello, World1, mytest app version: v1</p>"
+    return "<p>Hello, World1, mytest app version: v2</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
